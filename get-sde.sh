4c432a1ea38cf1fc0123c1890f5978fcabbdf351
Hendrik Albers <mail@hendrikalbers.de>
2018-04-23 20:16:33 +0200

    bash script to download and extract required yaml from SDE

---
+++ get-sde.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SDEURL=https://cdn1.eveonline.com/data/sde/tranquility/sde-20180323-TRANQUILITY.zip
+
+FILE=$(readlink -f $0)
+SCRIPT_DIR=$(dirname $FILE)
+
+pushd $SCRIPT_DIR > /dev/null
+
+echo "Downloading $SDEURL to sde.zip"
+wget $SDEURL -O sde.zip
+
+echo "Extracting required YAML files from sde.zip"
+unzip -j sde.zip sde/fsd/categoryIDs.yaml sde/fsd/groupIDs.yaml sde/fsd/typeIDs.yaml
+
+popd > /dev/null
